8966eee362f2e19f5759e7c75239c1ac2a54e975
Sher <76260479+shersafi@users.noreply.github.com>
2023-04-06 12:50:07 -0400

    added ability to customize graph (might change)

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,56 +1,94 @@
-function randomIP() {
-    var ip = (Math.floor(Math.random() * 255) + 1)+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255))+"."+(Math.floor(Math.random() * 255));
-    return ip;
+function createGraph() {
+  var numNodes = parseInt(document.getElementById("numNodes").value);
+  var edgeWeightsStr = document.getElementById("edgeWeights").value;
+  var edgeWeights = edgeWeightsStr.split(",");
+  var numEdges = edgeWeights.length;
+
+  var cy = cytoscape({
+    container: document.getElementById("cy"),
+    elements: {
+      nodes: initNodes(numNodes),
+      edges: initEdges(numEdges, edgeWeights),
+    },
+    style: [
+      {
+        selector: "node",
+        style: {
+          "background-image": "images/web-server-icon.png",
+          "background-fit": "cover",
+          width: "50px",
+          height: "50px",
+          "background-clip": "node",
+          shape: "rectangle",
+          "background-opacity": 0,
+          label: function (ele) {
+            return ele.data("label");
+          },
+          "text-margin-y": "-10px",
+        },
+      },
+      //   {
+      //       selector: 'edge#ab',
+      //       style: {
+      //           'line-color': 'red',
+      //       }
+      //   },
+      {
+        selector: "edge",
+        style: {
+          label: function (ele) {
+            return ele.data("weight");
+          },
+          "text-margin-y": "-20px",
+        },
+      },
+    ],
+  });
 }
 
-var cy = cytoscape({
-    container: document.getElementById('cy'),
-    elements: [
-      { data: { id: 'a', label: randomIP() } },
-      { data: { id: 'b', label: randomIP() } },
-      { data: { id: 'c', label: randomIP() } },
-      { data: { id: 'd', label: randomIP() } },
-      { data: { id: 'e', label: randomIP() } },
-      { data: { id: 'ab', source: 'a', target: 'b', weight: 3 }},
-      { data: { id: 'bc', source: 'b', target: 'c', weight: 7  }},
-      { data: { id: 'cd', source: 'c', target: 'e', weight: 1  }},
-      { data: { id: 'de', source: 'd', target: 'e', weight: 10  }},
-      { data: { id: 'ad', source: 'a', target: 'd', weight: 17  }},
-      { data: { id: 'bd', source: 'b', target: 'd', weight: 13  }}
-      ],
-      style: [
-          {
-              selector: 'node',
-              style: {
-                  'background-image': 'images/web-server-icon.png',
-                  'background-fit': 'cover',
-                  'width': '50px',
-                  'height': '50px',
-                  'background-clip': 'node',
-                  'shape': 'rectangle',
-                  'background-opacity': 0,
-                  'label': function(ele) {
-                    return ele.data('label');
-                  },
-                  'text-margin-y': '-10px'
-              }
-          },
-          {
-              selector: 'edge#ab',
-              style: {
-                  'line-color': 'red',
-              }
-          },
-          {
-              selector: 'edge',
-              style: {
-                  'label': function(ele) {
-                      return ele.data('weight');
-                  },
-                  'text-margin-y': '-20px'
-                  
-              }
-          }
-          
-      ]
-  });
+function randomIP() {
+  let ip =
+    Math.floor(Math.random() * 255) +
+    1 +
+    "." +
+    Math.floor(Math.random() * 255) +
+    "." +
+    Math.floor(Math.random() * 255) +
+    "." +
+    Math.floor(Math.random() * 255);
+  return ip;
+}
+
+function initEdges(numEdges, edgeWeights) {
+  let edges = [];
+  let edgeIndex = 1;
+
+  for (var i = 1; i <= numEdges; i++) {
+    let sourceNodeIndex = i;
+    let targetNodeIndex = (i % numEdges) + 1;
+    var weight = parseInt(edgeWeights[i - 1]);
+
+    edges.push({
+      data: {
+        id: "e" + edgeIndex,
+        source: "n" + sourceNodeIndex,
+        target: "n" + targetNodeIndex,
+        weight: weight,
+      },
+    });
+
+    edgeIndex++;
+  }
+
+  return edges;
+}
+
+function initNodes(numNodes) {
+  let nodes = [];
+
+  for (var i = 1; i <= numNodes; i++) {
+    nodes.push({ data: { id: "n" + i, label: randomIP() } });
+  }
+
+  return nodes;
+}
